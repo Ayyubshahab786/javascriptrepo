0b8a32ae27ef084d6b9a48efc29363d2eede356a
ayubshahab63 <ayubshahab63@gmail.com>
2020-07-17 13:27:43 +0500

    1st practice of scripting language and function console and alert

---
+++ index.js
@@ -0,0 +1,4 @@
+alert("hello world open javascript");
+alert("again")
+console.log("hello javascript") //node.js is used in server side 
+
---
+++ indexsl.js
@@ -0,0 +1,6 @@
+console.log("hello server side");
+console.log(2+8);
+// document.write("this statement is to see document write fuction")
+var name = "shahab"
+name = "shahab ayyub"
+console.log(name)
